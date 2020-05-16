802133f55dfdf3fa250a6bfae4e1bb22ebe2ec1b
dev4java11 <dev4java11@gmail.com>
2020-05-16 12:07:53 -0500

    Initial commit.

---
+++ src/test/java/pe/com/dev4java11/idperu/DemoIdperuApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.dev4java11.idperu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIdperuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
